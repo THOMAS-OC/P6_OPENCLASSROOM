e8754c607731504a30b8bed29723625caea41ee7
= <=>
2022-07-30 16:23:51 +0200

    amélioration de la route like : gestion des doublons de likes cote back end

--- back/controllers/sauceController.js
+++ back/controllers/sauceController.js
@@ -1,8 +1,10 @@
 const jwt = require('jsonwebtoken');
 const path = require("path")
+const fs = require("fs")
 // Importation des models
 const Sauce = require('../models/sauceModel');
 const User = require('../models/userModel');
+const { log } = require('console');
 
 
 // CREATE request OK
@@ -43,58 +45,134 @@ const updateLike = (req, res) => {
     console.log("nombre ajouté au like : " + req.body.like); // chiffre de -1 à 1
     // trouvé la sauce
     Sauce.findById(req.params.id, (err, doc) => {
+
         if (err){
             res.status(400).json({ err })
         }
+
         else {
+
+            // LIKE
             if (req.body.like == 1){
-                // Le supprimer de la liste des dislikes si l'utilisateur y est
+
+                // PRESENCE D'UN DISLIKE DANS LA BDD
                 if (doc.usersDisliked.includes(req.body.userId)) {
+
+                    // suppression du dislike
                     console.log("L'utilisateur est dans les dislikes");
                     let userIndex = doc.usersDisliked.indexOf(req.body.userId)
                     doc.usersDisliked.splice(userIndex, 1)
+
+                    // ajout du like
+                    doc.usersLiked.push(req.body.userId)
+
+                    // Mise à jour du nombre de likes et de dislikes
+                    doc.dislikes = doc.usersDisliked.length
+                    doc.likes = doc.usersLiked.length
+
+                    // Return
+                    doc.save()
+                    res.status(201).json(doc)
                 }
 
-                doc.usersLiked.push(req.body.userId)
+
+                // DOUBLON DE LIKE
+                else if (doc.usersLiked.includes(req.body.userId)){
+                    res.status(401).json({message : "Vous n'êtes pas autorisé à liker deux fois"})
+                }
+
+                // CAS NOMINAL
+
+                else {
+                    // ajout du like
+                    doc.usersLiked.push(req.body.userId)
+
+                    // Mise à jour du nombre de likes et de dislikes
+                    doc.dislikes = doc.usersDisliked.length
+                    doc.likes = doc.usersLiked.length
+
+                    // Return
+                    doc.save()
+                    res.status(201).json(doc)
+                }
 
                 // Mise à jour du nombre de likes et de dislikes
                 doc.dislikes = doc.usersDisliked.length
                 doc.likes = doc.usersLiked.length
+
             }
 
+            // REMOVE LIKE OR DISLIKE
             else if (req.body.like == 0){
+
+                // suppression de like
                 if (doc.usersLiked.includes(req.body.userId)) {
                     let userIndex = doc.usersLiked.indexOf(req.body.userId)
                     doc.usersLiked.splice(userIndex, 1)
                     doc.likes = doc.usersLiked.length // MAJ DU NOMBRE DE LIKES
                 }
+                
+                // suppression de dislike
                 else {
                     let userIndex = doc.usersDisliked.indexOf(req.body.userId)
                     doc.usersDisliked.splice(userIndex, 1) 
                     doc.dislikes = doc.usersDisliked.length // MAJ DU NOMBRE DE DISLIKES
                 }
-            }
 
-            else {
-                // Le supprimer de la liste des likes si l'utilisateur y est
-                if (doc.usersLiked.includes(req.body.userId)) {
-                    console.log("L'utilisateur est dans les likes");
-                    let userIndex = doc.usersLiked.indexOf(req.body.userId)
-                    doc.usersLiked.splice(userIndex, 1)
-                }
-                
-
-                // Ajouter l'utilisateur à la liste de dislikes
-                doc.usersDisliked.push(req.body.userId)
-                
                 // Mise à jour du nombre de likes et de dislikes
                 doc.dislikes = doc.usersDisliked.length
                 doc.likes = doc.usersLiked.length
+
+                // Return
+                doc.save()
+                res.status(201).json(doc)
+
             }
 
-            console.log(doc.likes);
-            doc.save()
-            res.status(201).json(doc)
+            // DISLIKE
+            else {
+
+                // PRESENCE D'UN LIKE DANS LA BDD
+                if (doc.usersLiked.includes(req.body.userId)) {
+
+                    console.log("L'utilisateur est dans les likes");
+                    let userIndex = doc.usersLiked.indexOf(req.body.userId)
+                    doc.usersLiked.splice(userIndex, 1)
+
+                    // Ajouter l'utilisateur à la liste de dislikes
+                    doc.usersDisliked.push(req.body.userId)
+                    
+                    // Mise à jour du nombre de likes et de dislikes
+                    doc.dislikes = doc.usersDisliked.length
+                    doc.likes = doc.usersLiked.length
+
+                    // Return
+                    doc.save()
+                    res.status(201).json(doc)
+                }
+
+                // Empêcher plusieurs dislike
+                else if (doc.usersDisliked.includes(req.body.userId)){
+                    res.status(401).json({message : "Vous n'êtes pas autorisé à disliker deux fois"})
+                }
+
+                // Cas nominal
+                else {
+
+                    // Ajouter l'utilisateur à la liste de dislikes
+                    doc.usersDisliked.push(req.body.userId)
+                    
+                    // Mise à jour du nombre de likes et de dislikes
+                    doc.dislikes = doc.usersDisliked.length
+                    doc.likes = doc.usersLiked.length
+
+                    // Return
+                    doc.save()
+                    res.status(201).json(doc)
+
+                }
+
+            }
         }
     })
 }
@@ -176,17 +254,27 @@ const deleteSauce = (req, res) => {
 
     Sauce.findOne({_id:req.params.id})
     .then(sauce => {
-        console.log("id depuis la sauce : " + sauce.userId)
+        
+        let filename = sauce.imageUrl;
+        filename = filename.split('/')
+        filename = filename[filename.length - 1] // name file image
+
+        let pathDelete = path.join(process.cwd(),'back/images/', filename)
+
         if (sauce.userId == req.auth.userId){
-            console.log("Autorisation de supprimer la sauce");
-            Sauce.deleteOne({ _id: req.params.id })
-            .then(() => res.status(200).json({ message: 'Sauce supprimé !'}))
-            .catch(error => res.status(400).json({ error }));
+            fs.unlink(pathDelete, () => {
+                console.log("Suppression autorisée");
+                Sauce.deleteOne({ _id: req.params.id })
+                .then(() => res.status(200).json({ message: 'Sauce supprimé !'}))
+                .catch(error => res.status(400).json({ error }));
+            })
         }
+
         else {
             res.status(403).json({message : "Utilisateur non autorisé"})
             console.log("non autorisé");
         }
+
     })
 
     .catch(err => console.log(err))
--- back/routes/loginRoute.js
+++ back/routes/loginRoute.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router()
 const controller = require('../controllers/loginController')
 
-
 router.post('/login', controller.login)
 
 router.post('/signup', controller.signup)
