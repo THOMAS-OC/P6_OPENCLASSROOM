f335944dd43e7cbecf9cd632c5459f3d78b5bec6
= <=>
2022-08-02 15:16:03 +0200

    Ajout du protocol https et implémentation fonctionnelle en localhost

--- back/app.js
+++ back/app.js
@@ -1,35 +1,39 @@
 const express = require("express")
-const app = express()
 const path = require("path");
 const mongoose = require('mongoose')
 const dotenv = require("dotenv")
-
-const helmet = require('helmet');
-
 dotenv.config()
+const cors = require("cors")
+const helmet = require('helmet');
+const https = require("https")
+const fs = require("fs")
+const app = express()
 
+app.use(cors())
 app.use(express.json());
 app.use(helmet());
 app.use(express.urlencoded({extended: false}));
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-    );
-    res.setHeader(
-      "Cross-Origin-Resource-Policy",
-      "cross-origin"
-    );
-    next();
-});
 
-// traitement du json
-// app.use(express.urlencoded({extended: true}));
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+//     );
+//     res.setHeader(
+//       "Access-Control-Allow-Methods",
+//       "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+//     );
+//     res.setHeader(
+//       "Cross-Origin-Resource-Policy",
+//       "cross-origin"
+//     );
+//     next();
+// });
+
+// Ajout HTTPS
+const key = fs.readFileSync(__dirname + "/" + "localhost-key.pem", "utf-8");
+const cert = fs.readFileSync(__dirname + "/" + "localhost.pem", "utf-8");
 
 // imporation des routeurs
 const sauceRoute = require('./routes/sauceRoute');
@@ -55,12 +59,8 @@ app.use('/api/sauces', sauceRoute)
 // Middleware route login
 app.use('/api/auth', loginRoute)
 
+https.createServer({ key, cert }, app).listen(3000);
 
-app.get("/", (req, res) => {
-    res.send("Page d'accueil")
-})
-
-
-app.listen(process.env.PORT, ()=>{
-    console.log("Lancement du serveur");
-})
+// app.listen(process.env.PORT, ()=>{
+//     console.log("Lancement du serveur");
+// })
